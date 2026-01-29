063299833aa542521ca3634dd42befa26b62fc85
Zhiyi Zhou <zhiyiz01@ucm.es>
2026-01-29 03:59:55 +0100

    hm scene 3 no existe

--- IG1App/Cube.cpp
+++ IG1App/Cube.cpp
@@ -1,8 +1,25 @@
 #include "Cube.h"
 #include "Mesh.h"
 
-Cube::Cube(GLdouble length, glm::vec4 color)
-	: SingleColorEntity(color)
+Cube::Cube(GLdouble length, glm::vec4 color): SingleColorEntity(color)
 {
 	mMesh = Mesh::generateCube(length);
+}
+
+void Cube::render(const glm::mat4& modelViewMat) const
+{
+	if (mMesh == nullptr) return;
+
+	glm::mat4 aMat = modelViewMat * mModelMat;
+
+	mShader->use();
+	mShader->setUniform("color", mColor);
+	upload(aMat);
+
+	glPolygonMode(GL_FRONT, GL_LINE);
+	glPolygonMode(GL_BACK, GL_POINT);
+
+	mMesh->render();
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
--- IG1App/SingleColorEntity.cpp
+++ IG1App/SingleColorEntity.cpp
@@ -21,11 +21,7 @@ void SingleColorEntity::render(const glm::mat4& modelViewMat) const {
 		mShader->setUniform("color", mColor);
 		upload(aMat);
 
-		glPolygonMode(GL_FRONT, GL_LINE);
-		glPolygonMode(GL_BACK, GL_POINT);
 
 		mMesh->render();
-
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	}
 }
