e230e3777bb652fd28d0b4ad361b8391ef69679b
Zhiyi Zhou <zhiyiz01@ucm.es>
2026-04-18 18:30:53 +0200

    Supuesto cono apartado 65

--- IG1App/Cone.cpp
+++ IG1App/Cone.cpp
@@ -8,10 +8,23 @@ Cone::Cone(GLdouble h, GLdouble r, GLdouble R, GLuint nRings, GLuint nSamples) {
 
     // R: radio arriba
     // r: radio abajo
+    // 
+    // cambio en radio
+    GLdouble dr = (R - r) / nRings;
+    // la altura
+    GLdouble dy= h/nRings;
+
+
+    // 
+    GLdouble y = -h * 0.5;
+    GLdouble radius = r;
 
     // crear la figura
     for (int i = 0; i < nRings; ++i)
     {
+        profile.emplace_back(radius, y);
+        radius += dr;
+        y += dy;
     }
 
     mMesh = IndexMesh::generateByRevolution(profile, nSamples);
--- IG1App/Scene7.cpp
+++ IG1App/Scene7.cpp
@@ -1,8 +1,12 @@
 #include "Scene7.h"
 #include "Sphere.h"
+#include "Cone.h"
 
 void Scene7::init() {
 	Scene::init();
-	Sphere* s = new Sphere(100, 10, 40);
-	gObjects.push_back(s);
+	/*Sphere* s = new Sphere(100, 10, 40);
+	gObjects.push_back(s);*/
+
+	Cone* c = new Cone(100, 80, 30, 10,10);
+	gObjects.push_back(c);
 }
