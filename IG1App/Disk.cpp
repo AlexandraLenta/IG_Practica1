6880b0f60af8f5af610221df8d4f06e11999d8ce
Zhiyi Zhou <zhiyiz01@ucm.es>
2026-04-18 19:10:30 +0200

    Disk apartado 65

--- IG1App/Disk.cpp
+++ IG1App/Disk.cpp
@@ -1,1 +1,19 @@
 #include "Disk.h"
+#include "IndexMesh.h"
+Disk::Disk(GLdouble R, GLdouble r, GLuint nRings, GLuint nSamples) {
+
+	std::vector<glm::vec2> profile;
+
+	profile.reserve(nRings + 1);
+	GLdouble radius = r;
+	// incrementa radio desde interior a exterior
+	GLdouble dr = (R - r) / nRings;
+
+	for (GLuint i = 0; i <= nRings; ++i)
+	{
+		profile.emplace_back(radius, 0.0);
+		radius += dr;
+	}
+
+	mMesh = IndexMesh::generateByRevolution(profile, nSamples);
+}
--- IG1App/Scene7.cpp
+++ IG1App/Scene7.cpp
@@ -1,12 +1,16 @@
 #include "Scene7.h"
 #include "Sphere.h"
 #include "Cone.h"
+#include "Disk.h"
 
 void Scene7::init() {
 	Scene::init();
 	/*Sphere* s = new Sphere(100, 10, 40);
 	gObjects.push_back(s);*/
 
-	Cone* c = new Cone(100, 80, 30, 10,10);
-	gObjects.push_back(c);
+	/*Cone* c = new Cone(100, 80, 30, 10,10);
+	gObjects.push_back(c);*/
+
+	Disk* d = new Disk(100, 30, 10, 10);
+	gObjects.push_back(d);
 }
