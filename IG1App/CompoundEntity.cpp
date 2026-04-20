9025d27fd3bf0c0b2adddf208ca5320c37df7258
Alexandra <alexlent@ucm.es>
2026-04-20 11:27:11 +0200

    Empezar apartado 66

---
+++ IG1App/CompoundEntity.cpp
@@ -0,0 +1,23 @@
+#include "CompoundEntity.h"
+
+CompoundEntity::CompoundEntity() {
+
+}
+CompoundEntity::~CompoundEntity() {
+
+}
+
+void CompoundEntity::addEntity(Abs_Entity* ae) {}
+
+void CompoundEntity::render(const glm::mat4& modelViewMat) const {
+
+}
+void CompoundEntity::update() {
+
+}
+void CompoundEntity::load() {
+
+}
+void CompoundEntity::unload() {
+
+}
---
+++ IG1App/CompoundEntity.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Entity.h"
+
+class CompoundEntity :
+    public Abs_Entity
+{
+public:
+    CompoundEntity();
+    ~CompoundEntity();
+
+    void addEntity(Abs_Entity* ae);
+
+    void render(const glm::mat4& modelViewMat) const override;
+    void update() override;
+    void load() override;
+    void unload() override;
+
+protected:
+    std::vector<Abs_Entity*> gObjects;
+};
+
