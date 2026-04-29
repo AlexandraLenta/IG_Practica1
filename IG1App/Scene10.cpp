a2e9536a075fdae27648a4361b8707c98a8aab58
Zhiyi Zhou <zhiyiz01@ucm.es>
2026-04-30 00:55:39 +0200

    Poner color

--- IG1App/Scene10.cpp
+++ IG1App/Scene10.cpp
@@ -16,4 +16,11 @@ void Scene10::init() {
     gObjects.push_back(goldPlanet);
     goldPlanet->setModelMat(glm::translate(glm::mat4(1.0f), glm::vec3(2 * r, 0, 0)));
 
+    Material m;
+    m.setAmb(glm::vec3(0.2f, 0.2f, 0.0f));
+    m.setDiff(glm::vec3(0.7f, 0.5f, 0.0f));
+    m.setSpec(glm::vec3(0.6f, 0.5f, 0.3f));
+    m.setExp(32);
+    goldPlanet->setMaterial(m);
+
 }
